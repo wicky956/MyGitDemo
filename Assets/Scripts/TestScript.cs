edcd7e3ac1d8473f8dbfdec0e80cc0ff8d00de2f
wicky956 <m.waqas@9dtechnologies.net>
2024-09-04 17:28:15 +0500

    Update TestScript.cs
    Update to the Test script

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("My Github Repo First change");
     }
 
     // Update is called once per frame
